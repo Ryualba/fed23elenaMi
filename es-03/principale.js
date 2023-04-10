404af3fa158dcde66295daad1c02b41b8fd0abae
Ryualba <127239690+Ryualba@users.noreply.github.com>
2023-04-10 11:11:00 +0200

    update

---
+++ es-03/principale.js
@@ -0,0 +1,12 @@
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    $('grid').isotope({
+          // options
+  itemSelector: '.grid-item',
+  layoutMode: 'fitRows',
+
+    });
+
+});
+
